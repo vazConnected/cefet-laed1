960e9d9896af6a42f0fb5bf1e66767792ac5ad8d
Pedro Vaz <holo_vaz@outlook.com>
2021-02-25 11:55:18 -0300

    Problema da mochila (forca bruta) - verificado

--- exercicios/04_oProblemaDaMochila.c
+++ exercicios/04_oProblemaDaMochila.c
@@ -2,12 +2,12 @@
  * Autores: Pedro Vaz e Ana Velasque
  * Disciplina Laboratorio de Algoritmos e Estruturas de Dados I (LAED I)
  * 
- * Prazo para a entrega: 1
+ * Prazo para a entrega: 26/02/2021 23:55:55
  * Plataforma: run.codes
  * 
  * Titulo: O Problema da Mochila
  * Descricao: o qualidade da solucao do problema de armazenar o maior
- *   valor agragado de produtos em uma mochila respeitando sua capacidade
+ *   valor agregado de produtos em uma mochila respeitando sua capacidade
  *   de peso
  * 
  */
@@ -22,85 +22,81 @@ typedef struct{
     int peso;
 } Item;
 
-/*
- * Retorna falso se a escolha realizada e invalida ou pior que a do jogador
- * Legenda : 
- *   EJ : escolha do jogador
- *   EA : escolha atual
- */
-bool analisaEscolha(int capacidadeMochila, int valorEJ, int valorEA, int pesoEA, Item* itens, int inicioArray, int tamanho){
-    if(pesoEA > capacidadeMochila){ // Capacidade da mochila excedida
-        return false;
-    }else if(inicioArray == tamanho - 1){ // Se for o ultimo elemento possivel
-        if(valorEA + itens[inicioArray].valor > valorEJ && pesoEA + itens[inicioArray].peso <= capacidadeMochila){ // Se o valor nao for bom o suficiente
-            return true; // Escolha melhor que a do jogador
+// Valor para a solucao otima - VERIFICADO
+int valorSolucaoOtima(int capacidadeRestante, Item* itens, int inicioArray, int tamanhoArray){
+    
+    // Array percorrido por completo - VERIFICADO
+    if(inicioArray == tamanhoArray){
+        return 0;
+    }
+
+    // Mochila cheia - VERIFICADO
+    if(capacidadeRestante <= 0){
+        return 0;
+    }
+
+    int valorSemEscolha = valorSolucaoOtima(capacidadeRestante, itens, inicioArray + 1, tamanhoArray);
+    int valorComEscolha = 0;
+
+    if(itens[inicioArray].peso <= capacidadeRestante){
+        capacidadeRestante -= itens[inicioArray].peso;
+        valorComEscolha = itens[inicioArray].valor + valorSolucaoOtima(capacidadeRestante, itens, inicioArray + 1, tamanhoArray);
+        
+        if(valorComEscolha > valorSemEscolha){
+            return valorComEscolha;
         }
-        return false;
     }
 
-    inicioArray++;
-
-    // Item atual nao escolhido
-    bool escolha0 = analisaEscolha(capacidadeMochila, valorEJ, valorEA, pesoEA, itens, inicioArray, tamanho);
-
-    // Item atual escolhido
-    valorEA += itens[inicioArray].valor;
-    pesoEA += itens[inicioArray].peso;
-    if(pesoEA > capacidadeMochila){ // Se utrapassar a capacidade da mochila
-        return false;
-    }
-    bool escolha1 = analisaEscolha(capacidadeMochila, valorEJ, valorEA, pesoEA, itens, inicioArray, tamanho);
-
-    if(escolha0 || escolha1){ // Alguma das escolhas e viavel e melhor que a do jogador
-        return true;
-    }
+    return valorSemEscolha;
 }
 
 int main(){
     char* caminhoParaArquivo = (char*) malloc(4096 * sizeof(char));
 
+    // Nome do arquivo - VERIFICADO
     printf("Digite o nome do arquivo: ");
     scanf("%s", caminhoParaArquivo);
     caminhoParaArquivo = (char*) realloc(caminhoParaArquivo, strlen(caminhoParaArquivo));
-
     FILE* arquivo = fopen(caminhoParaArquivo, "r");
 
+    // Capacidade da mochile e qtd de itens na loja - VERIFICADO
     int capacidadeDaMochila, qtdItensDaLoja;
     fscanf(arquivo, "%d", &capacidadeDaMochila);
     fscanf(arquivo, "%d", &qtdItensDaLoja);
 
     Item* itensDaLoja = (Item*) malloc(qtdItensDaLoja * sizeof(Item));
 
+    // Leitura dos itens da loja - VERIFICADO
     for(int i = 0; i < qtdItensDaLoja; i++){
         fscanf(arquivo, "%d", &itensDaLoja[i].peso);
         fscanf(arquivo, "%d", &itensDaLoja[i].valor);
     }
 
-    int valorTotal = 0;
-    int pesoTotal = 0;
-    char* escolhaRealizada = (char*) calloc(qtdItensDaLoja, sizeof(char));
+    int valorEscolhaJogador = 0, pesoEscolhaJogador = 0;
+    int* escolhaRealizada = (int*) calloc(qtdItensDaLoja, sizeof(int));
 
+    // Valor total e peso total - VERIFICADO
     for(int i = 0; i < qtdItensDaLoja; i++){
-        fscanf(arquivo, "%c", &escolhaRealizada[i]);
-        if(escolhaRealizada[i] == '1'){
-            valorTotal += itensDaLoja[i].valor;
-            pesoTotal += itensDaLoja[i].peso;
+        fscanf(arquivo, "%d", &escolhaRealizada[i]);
+        if(escolhaRealizada[i] == 1){
+            valorEscolhaJogador += itensDaLoja[i].valor;
+            pesoEscolhaJogador += itensDaLoja[i].peso;
         }
     }
 
     fclose(arquivo);
 
-    if(pesoTotal > capacidadeDaMochila){
+    // Solucao Inviavel - VERIFICADO
+    if(pesoEscolhaJogador > capacidadeDaMochila){ 
         printf("Solucao inviavel.");
         return 0;
     }
-
-    char* possivelSolucaoOtima = (char*) malloc( qtdItensDaLoja * sizeof(char) );
-
-    bool solucaoViavelNaoOtima = analisaEscolha(capacidadeDaMochila, valorTotal, 0, 0, itensDaLoja, 0, qtdItensDaLoja);
-
-    if(solucaoViavelNaoOtima){
-        printf("Solucao viavel mas nao otima."); 
+    
+    // Valor Para Melhor Escolha - VERIFICADO
+    int valorEscolhaOtima = valorSolucaoOtima(capacidadeDaMochila, itensDaLoja, 0, qtdItensDaLoja);
+    
+    if(valorEscolhaJogador < valorEscolhaOtima){
+        printf("Solucao viavel mas nao otima.");
     }else{
         printf("Solucao otima.");
     }
